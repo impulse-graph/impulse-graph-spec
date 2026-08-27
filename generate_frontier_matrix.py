1cabd5c0ce75f2bc4fad899b93cf9022a27997fa
Jesse Granden <jesse.granden@gmail.com>
2026-08-26 22:50:44 -0500

    Fix test expectations and generator logic for poly sets

--- generate_frontier_matrix.py
+++ generate_frontier_matrix.py
@@ -3,11 +3,11 @@ import os
 opcodes = {
     "node_filter": {
         "op": "OP_NODE_FILTER",
-        "fmt": "{op} R4, R3, R0, 0", # R4=dest, R3=frontier, R0=data
+        "fmt": "{op} R4, R3, R0, 0, 0",
         "setup": """
-.array_float MOCK_PRICES = [1.5, 0.0, 3.14, 0.0]
-0x00: OP_LOAD_INLINE_ARRAY     R0, MOCK_PRICES
-0x01: OP_INIT_MOCK_NODE_ATTR   0, R0, R255
+0x00: OP_LOAD_INLINE_INT_ARRAY R1, MOCK_VALUES
+0x01: OP_INIT_MOCK_NODE_ATTR   0, R1, R255
+0x02: OP_LOAD_CONST_INT        R0, 30
 """
     }
 }
@@ -15,15 +15,15 @@ opcodes = {
 representations = {
     "id_list": {
         "data": ".array_node FRONTIER = [0, 2]",
-        "load": "0x02: OP_LOAD_INLINE_NODE_ARRAY  R3, FRONTIER"
+        "load": "0x03: OP_LOAD_INLINE_NODE_ARRAY  R3, FRONTIER"
     },
     "roaring": {
         "data": ".set_roaring FRONTIER = [0, 2]",
-        "load": "0x02: OP_LOAD_INLINE_SET_ROARING R3, FRONTIER"
+        "load": "0x03: OP_LOAD_INLINE_SET_ROARING R3, FRONTIER"
     },
     "dense": {
         "data": ".set_dense FRONTIER = [0, 2]",
-        "load": "0x02: OP_LOAD_INLINE_SET_DENSE   R3, FRONTIER"
+        "load": "0x03: OP_LOAD_INLINE_SET_DENSE   R3, FRONTIER"
     }
 }
 
@@ -33,15 +33,18 @@ template = """
 ; ====================================================================
 ; {{EXPECT: STATUS = IMPULSE_VM_OK}}
 ; {{EXPECT: FLAG = !ZF}}
+; {{EXPECT: R5 = 1}}
 
 .data
+.array_int MOCK_VALUES = [10, 0, 30, 0]
 {data_decl}
-{op_setup}
 
 .text
+{op_setup}
 {load_inst}
-0x03: {exec_inst}
-0x04: OP_HALT
+0x04: {exec_inst}
+0x05: OP_SET_CARDINALITY R5, R4
+0x06: OP_HALT
 """
 
 for op_name, op_info in opcodes.items():
@@ -52,7 +55,7 @@ for op_name, op_info in opcodes.items():
         content = template.format(
             test_name=test_name.upper(),
             data_decl=rep_info["data"],
-            op_setup=op_info["setup"],
+            op_setup=op_info["setup"].strip(),
             load_inst=rep_info["load"],
             exec_inst=op_info["fmt"].format(op=op_info["op"])
         )
